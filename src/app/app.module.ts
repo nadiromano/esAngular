3b790470044a8f734448328da0390381656dc530
giuseppe romano <giuseppenadir.romano@gmail.com>
2022-02-14 19:59:54 +0100

    esercizio 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserContainerComponent } from './user-container/user-container.component';
+import { UserListComponent } from './user-container/user-list/user-list.component';
+import { UserSingleComponent } from './user-container/user-list/user-single/user-single.component';
+import { UserDetailComponent } from './user-container/user-detail/user-detail.component';
+import { OrderContainerComponent } from './order-container/order-container.component';
+import { OrderListComponent } from './order-container/order-list/order-list.component';
+import { OrderSingleComponent } from './order-container/order-list/order-single/order-single.component';
+import { OrderDetailComponent } from './order-container/order-list/order-single/order-detail/order-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserContainerComponent,
+    UserListComponent,
+    UserSingleComponent,
+    UserDetailComponent,
+    OrderContainerComponent,
+    OrderListComponent,
+    OrderSingleComponent,
+    OrderDetailComponent
   ],
   imports: [
     BrowserModule
