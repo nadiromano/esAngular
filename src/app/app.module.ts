49b3ce37def4358629fc2d068a2267ce6bab5d12
giuseppe romano <giuseppenadir.romano@gmail.com>
2022-02-15 16:21:52 +0100

    es6 fine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { UserListComponent } from './user-list/user-list.component';
+import { UserSingleComponent } from './user-list/user-single/user-single.component';
 
 @NgModule({
-  declarations: [AppComponent, UserListComponent],
+  declarations: [AppComponent, UserListComponent, UserSingleComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/user-list/user-single/user-single.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+
+@Component({
+  selector: 'app-user-single',
+  templateUrl: './user-single.component.html',
+  styleUrls: ['./user-single.component.css'],
+})
+export class UserSingleComponent implements OnInit {
+  @Input() users: User[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
